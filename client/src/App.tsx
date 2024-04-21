49a367caf5dfa357e717ddf310d36b47be47b90a
Najwa <najwa.kv20@gmail.com>
2024-04-21 17:19:53 +0530

    Designed and implemented the UI for the signup component.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,11 @@
-
+import RegisterForm from "./components/RegisterForm";
 
 function App() {
-
   return (
-    <>
-      
-    </>
-  )
+    <div className="App">
+      <RegisterForm/>
+    </div>
+  );
 }
 
-export default App
+export default App;
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,6 +2,23 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    colors: {
+      purple: "#3A244A",
+      red: "#D72638",
+      white: "#FFFFFF",
+      gray: "#E0E0E0",
+      black: "#69615f",
+      transparent: "#00000000",
+    },
+    fontFamily: {
+      roboto: ["Roboto", "sans-serif"],
+    },
+    screens: {
+      'tablet': '768px',
+      'laptop': '1024px',
+      'desktop': '1280px',
+      'monitor': '1536px',
+    },
     extend: {},
   },
   plugins: [],
