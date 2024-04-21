76cefda3f4fcbc8981e5862a386351eaad2ed6e7
Najwa <najwa.kv20@gmail.com>
2024-04-21 19:57:58 +0530

    Implemented seperate component for Form Heading.

---
+++ client/src/components/Heading.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+interface HeadingProps {
+  title: string;
+}
+const Heading: React.FC<HeadingProps> = ({title}) => {
+  return (
+    <h2 className="text-2xl laptop:text-3xl desktop:text-4xl monitor:text-5xl font-bold">
+      <span className="text-purple">{title}</span>
+      <span className="text-red">!</span>
+    </h2>
+  );
+};
+
+export default Heading;
